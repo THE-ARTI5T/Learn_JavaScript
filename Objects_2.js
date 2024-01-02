9249e3af26842e51f8dfaa27cf09ced142f15320
Naman <sri.naman07@gmail.com>
2024-01-02 22:41:04 +0530

    Spread Operator and Merge Ojects

--- Objects_2.js
+++ Objects_2.js
@@ -17,8 +17,24 @@ const regUser = {
     }
 }
 
-console.log(regUser);
-console.log(regUser.name);
-console.log(regUser.name.fullName);
-console.log(regUser.name.fullName.firstName);
-console.log(regUser.name.fullName.lastName);
+// console.log(regUser);
+// console.log(regUser.name);
+// console.log(regUser.name.fullName);
+// console.log(regUser.name.fullName.firstName);
+// console.log(regUser.name.fullName.lastName);
+
+
+// Spread Operator (...)
+
+const obj1 = { 1: 'a', 2: 'b' }
+const obj2 = { 3: 'c', 4: 'd' }
+
+// Two ways to merge objects
+// 1.
+const obj3 = Object.assign({}, obj1, obj2)
+
+// 2.
+const obj4 = { ...obj1, ...obj2 };
+
+console.log(obj3)
+console.log(obj4)
