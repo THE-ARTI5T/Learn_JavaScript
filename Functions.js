69f36cf9355af55d399b8b6a82d07488c90f9211
Naman <sri.naman07@gmail.com>
2024-01-03 20:17:08 +0530

    Functions, Scopes and Rest Operator

--- Functions.js
+++ Functions.js
@@ -5,3 +5,26 @@ addNum("3", 4)
 addNum("a", 4)
 addNum(3, "4")
 addNum(3, 4)
+
+
+// Rest Operator (...)
+
+// when the size of input is not defined we use rest operator
+function calculateCart(...num1) {
+    return num1
+}
+console.log(calculateCart(200, 100, 400, 1))
+
+
+
+function onef() {
+    const name = "Naman"
+    const name1 = "Namanupd"
+    function twof() {
+        const naam = "Sri"
+        console.log(name);
+    }
+    console.log(name1);
+    twof()
+}
+onef()
